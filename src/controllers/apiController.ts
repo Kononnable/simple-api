95e383964f3aba787f1f67c1ce38a1f4607d6395
Kononnable <kononnable@gmail.com>
2019-04-14 20:53:13 +0200

    project initial configuration files

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+export function apiController(router: Router) {
+    return router;
+}
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express = require('express');
+import bodyParser = require('body-parser');
+import {apiController} from './controllers/apiController'
+
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use('/api', apiController(router));
+
+app.listen(port);
+console.log(`App listening on port ${port}!`)
