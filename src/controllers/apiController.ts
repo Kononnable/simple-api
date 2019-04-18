59a7fe0a931bf0c5d4ae6431b31d9176330d60d4
Kononnable <kononnable@gmail.com>
2019-04-18 19:55:12 +0200

    refactoring

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -13,78 +13,63 @@ import { RequestWithPayload } from "../models/RequestWithPayload";
 export function apiController(router: Router) {
     const generatedKeys = {} as any;
 
-    router.post("/sign-in", signIn());
-    router.post("/generate-key-pair", authMidlleware, generateKeyPair());
-    router.post("/encrypt", authMidlleware, encrypt());
+    router.post("/sign-in", signIn);
+    router.post("/generate-key-pair", authMidlleware, generateKeyPair);
+    router.post("/encrypt", authMidlleware, encrypt);
     return router;
 
-    function signIn() {
-        return function(req: Request, res: Response) {
-            const validationResult = Joi.validate(req.body, SignInRequest);
-            if (validationResult.error) {
-                res.status(400).send(validationResult.error.message);
-            } else {
-                const body = req.body as ISignInRequest;
-                if (
-                    Users.some(
-                        v =>
-                            v.email === body.email &&
-                            v.password === body.password
-                    )
-                ) {
-                    const token = jwt.sign({ email: body.email }, Secret, {
-                        expiresIn: "5m"
-                    });
-                    res.json({
-                        authToken: token
-                    });
-                } else {
-                    res.sendStatus(401);
-                }
-            }
-        };
-    }
-
-    function generateKeyPair() {
-        return function(req: RequestWithPayload, res: Response) {
-            const { privateKey, publicKey } = crypto.generateKeyPairSync(
-                "rsa",
-                {
-                    modulusLength: 2048,
-                    publicKeyEncoding: {
-                        type: "spki",
-                        format: "pem"
-                    },
-                    privateKeyEncoding: {
-                        type: "pkcs8",
-                        format: "pem",
-                        cipher: "aes-256-cbc",
-                        passphrase: Secret2
-                    }
-                }
-            );
-            generatedKeys[req.payload!.email] = publicKey;
-            res.json({ privKey: privateKey, pubKey: publicKey });
-        };
-    }
-
-    function encrypt() {
-        return async function(req: RequestWithPayload, res: Response) {
-            if (generatedKeys[req.payload!.email]) {
-                const filePath = path.resolve(
-                    process.cwd(),
-                    `data`,
-                    `sample.pdf`
-                );
-                const readFilePromisified = promisify(fs.readFile);
-                const fileContent = await readFilePromisified(filePath);
-                res.send({
-                    fileContent: fileContent.toString("base64"),
-                    publicKey: generatedKeys[req.payload!.email]
+    function signIn(req: Request, res: Response) {
+        const validationResult = Joi.validate(req.body, SignInRequest);
+        if (validationResult.error) {
+            res.status(400).send(validationResult.error.message);
+        } else {
+            const body = req.body as ISignInRequest;
+            if (
+                Users.some(
+                    v => v.email === body.email && v.password === body.password
+                )
+            ) {
+                const token = jwt.sign({ email: body.email }, Secret, {
+                    expiresIn: "5m"
+                });
+                res.json({
+                    authToken: token
                 });
             } else {
-                res.sendStatus(400);
+                res.sendStatus(401);
             }
-        };
+        }
+    }
+
+    function generateKeyPair(req: RequestWithPayload, res: Response) {
+        const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+            modulusLength: 2048,
+            publicKeyEncoding: {
+                type: "spki",
+                format: "pem"
+            },
+            privateKeyEncoding: {
+                type: "pkcs8",
+                format: "pem",
+                cipher: "aes-256-cbc",
+                passphrase: Secret2
+            }
+        });
+        generatedKeys[req.payload!.email] = publicKey;
+        res.json({ privKey: privateKey, pubKey: publicKey });
+    }
+
+    async function encrypt(req: RequestWithPayload, res: Response) {
+        if (generatedKeys[req.payload!.email]) {
+            const filePath = path.resolve(process.cwd(), `data`, `sample.pdf`);
+            const readFilePromisified = promisify(fs.readFile);
+            const fileContent = await readFilePromisified(filePath);
+            res.send({
+                fileContent: fileContent.toString("base64"),
+                publicKey: generatedKeys[req.payload!.email]
+            });
+        } else {
+            res.sendStatus(400);
+        }
     }
 }
